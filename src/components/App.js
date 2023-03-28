2895984de1f48eb1181a73b60fdf4168272102d1
Jagdeesh Kumar <121223763+jagdeesh97@users.noreply.github.com>
2023-03-28 22:50:09 +0530

    movie-search-project

--- src/components/App.js
+++ src/components/App.js
@@ -1,36 +1,48 @@
-import React, { useState } from "react";
-import "../styles/App.css";
-import { movies } from "../utils/movieList";
+import React, { useState } from 'react';
+import '../styles/App.css';
+import { movies } from '../utils/movieList';
 
 const App = () => {
-    const [inputValue, setInputValue] = useState("");
-    const [results, setResult] = useState(null);
+	const [inputValue, setInputValue] = useState('');
+	const [results, setResult] = useState(null);
 
-    function handleClick(e) {}
+	function handleClick(e) {
+		e.preventDefault();
 
-    function handleChange(e) {}
+		const arr = [];
+		movies.map(m => {
+			if (m?.title?.toLowerCase() === inputValue?.toLowerCase()) {
+				arr.push(m.title);
+			}
+		});
+		setResult(arr);
+	}
 
-    return (
-        <div id="main">
-            <form id="form">
-                <input
-                    type="text"
-                    onChange={handleChange}
-                    value={inputValue}
-                    id="movie-input"
-                ></input>
-                <button id="button" onClick={handleClick}>
-                    Search
-                </button>
-            </form>
-            <div id="result">
-                {results &&
-                    results.map((movie) => (
-                        <div className="movie">{movie}</div>
-                    ))}
-            </div>
-        </div>
-    );
+	function handleChange(e) {
+		setInputValue(e.target.value);
+	}
+
+	return (
+		<div id="main">
+			<form id="form">
+				<input
+					type="text"
+					onChange={handleChange}
+					value={inputValue}
+					id="movie-input"></input>
+				<button id="button" onClick={handleClick}>
+					Search
+				</button>
+			</form>
+			<div id="result">
+				{!results ? (
+					<div className="movie"></div>
+				) : (
+					results.map(movie => <div className={'movie'}>{movie}</div>)
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default App;
